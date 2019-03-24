97e8c7ea254483d47e82a926d711c4c3549e0325
Brandon Ling <brandonvling@gmail.com>
2019-03-24 16:44:46 -0400

    Add docstring

--- chapter_1/Exercise_1-12.scm
+++ chapter_1/Exercise_1-12.scm
@@ -1,4 +1,11 @@
 (define (pascals_triangle n m)
+    ;;;
+    ; returns the m-th element corresponding to the n-th row of Pascal's triangle
+    ; param n: integer > 0
+    ; param m: 0 < integer  <= n
+    ; return: integer
+    ;;;
+
     (cond ((= m 1) 1)
           ((= m n) 1)
           (else (+ (pascals_triangle (- n 1) (- m 1)) 
