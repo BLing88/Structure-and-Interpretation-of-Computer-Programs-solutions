d1dfab5f021e9fc6aee2c1699c7e7e351ffd6d15
Brandon Ling <brandonvling@gmail.com>
2019-08-17 17:26:36 -0400

    Add solution to exercise 1.21

---
+++ chapter_1/Exercise_1-21.scm
@@ -0,0 +1,15 @@
+(define (square n)
+    (* n n))
+
+(define (smallest-divisor n)
+    (define (find-divisor n test-divisor)
+        (define (divides? a b)
+            (= (remainder b a) 0))
+        (cond ((> (square test-divisor) n) n)
+            ((divides? test-divisor n) test-divisor)
+            (else (find-divisor n (+ test-divisor 1)))))
+    (find-divisor n 2))
+
+; (smallest-divisor 199)
+; (smallest-divisor 1999)
+(smallest-divisor 19999)
