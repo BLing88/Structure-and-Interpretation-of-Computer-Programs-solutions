9b1d8a28f55621d21ab42e97d1ecb3a96736bd5e
Brandon Ling <brandonvling@gmail.com>
2019-08-18 11:52:22 -0400

    Add working version of search-for-primes

--- chapter_1/Exercise_1-22.scm
+++ chapter_1/Exercise_1-22.scm
@@ -25,3 +25,21 @@
 (define (report-prime elapsed-time)
     (display "***")
     (display elapsed-time))
+
+; (timed-prime-test 333334373)
+; (prime? 333334373)
+
+(define (search-for-primes start end number-of-primes)
+    (define (prime-search-iter n counter)
+        (cond ((prime? n) (timed-prime-test n)))
+        (if (or (= counter number-of-primes) (> n end))
+            "finished"
+            (if (prime? n)
+                (prime-search-iter (+ n 1) (+ counter 1))
+                (prime-search-iter (+ n 1) counter))))
+    (prime-search-iter start 0))
+
+ (search-for-primes 1000000000000 200000000021 3)       ; 1e9 
+ (search-for-primes 10000000000000 20000000000061 3)     ; 1e10 
+ (search-for-primes 100000000000000 100000000000057 3)   ; 1e11 
+ (search-for-primes 1000000000000000 1000000000000063 3) ; 1e12 
