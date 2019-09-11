927f0bee3b2ed5d3ecf732934bd0140882cdd7ce
Brandon Ling <brandonvling@gmail.com>
2019-09-10 23:05:41 -0400

    Format to pretty printing format

--- chapter_1/Exercise_1-29.scm
+++ chapter_1/Exercise_1-29.scm
@@ -4,12 +4,22 @@
         (if (= counter 0)
             sum
             (if (= (remainder counter 2) 0)
-                (iter (+ sum (* 2 (f (+ a (* counter h))))) (- counter 1))
-                (iter (+ sum (* 4 (f (+ a (* counter h))))) (- counter 1))
+                (iter (+ sum 
+                         (* 2 (f (+ a 
+                                    (* counter h))))) 
+                      (- counter 1))
+                (iter (+ sum 
+                         (* 4 (f (+ a 
+                                    (* counter h))))) 
+                      (- counter 1))
             )
         )
     )
-    (/ (* h (iter (+ (f a) (f b)) (- n 1))) 3)
+    (/ (* h 
+          (iter (+ (f a) 
+                   (f b)) 
+                (- n 1))) 
+        3)
 )
 
 (define (cube x) (* x x x ))
