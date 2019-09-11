89c2eb4b1f97709ca18be4d73ee0a84b3d70d0d2
Brandon Ling <brandonvling@gmail.com>
2019-09-10 22:59:38 -0400

    Add solution to 1.29

---
+++ chapter_1/Exercise_1-29.scm
@@ -0,0 +1,17 @@
+(define (simpsons-rule f a b n)
+    (define h (/ (- b a) n))
+    (define (iter sum counter)
+        (if (= counter 0)
+            sum
+            (if (= (remainder counter 2) 0)
+                (iter (+ sum (* 2 (f (+ a (* counter h))))) (- counter 1))
+                (iter (+ sum (* 4 (f (+ a (* counter h))))) (- counter 1))
+            )
+        )
+    )
+    (/ (* h (iter (+ (f a) (f b)) n)) 3)
+)
+
+(define (cube x) (* x x x ))
+
+(simpsons-rule cube 0 1 100000)
