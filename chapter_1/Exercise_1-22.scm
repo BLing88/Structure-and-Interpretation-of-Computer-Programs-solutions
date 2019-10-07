18ef0a0da0528f36c7784e49a50a17f545a3359b
Brandon Ling <brandonvling@gmail.com>
2019-10-07 18:17:46 -0400

    Reimplement search-for-primes more efficiently

--- chapter_1/Exercise_1-22.scm
+++ chapter_1/Exercise_1-22.scm
@@ -20,24 +20,35 @@
 
 (define (start-prime-test n start-time)
     (if (prime? n)
-        (report-prime (- (runtime) start-time))))
+        (and (report-prime n (- (runtime) start-time)) #t)
+        #f))
 
-(define (report-prime elapsed-time)
-    (display "***")
+(define (report-prime n elapsed-time)
+    (newline)
+    (display n)
+    (display " *** ")
     (display elapsed-time))
 
 ; (timed-prime-test 333334373)
 ; (prime? 333334373)
 
-(define (search-for-primes start end number-of-primes)
+(define (search-for-primes start end number-primes)
     (define (prime-search-iter n counter)
-        (cond ((prime? n) (timed-prime-test n)))
-        (if (or (= counter number-of-primes) (> n end))
-            "finished"
-            (if (prime? n)
-                (prime-search-iter (+ n 1) (+ counter 1))
-                (prime-search-iter (+ n 1) counter))))
+        (cond ((or (> n end) (= counter number-primes)) (and (newline) (display "finished search")))
+            ((= (remainder n 2) 0) (prime-search-iter (+ n 1) counter))
+            ((timed-prime-test n) (prime-search-iter (+ n 2) (+ counter 1)))
+            (else (prime-search-iter (+ n 2) counter))))
     (prime-search-iter start 0))
+
+; (define (search-for-primes start end number-of-primes)
+;     (define (prime-search-iter n counter)
+;         (cond ((prime? n) (timed-prime-test n)))
+;         (if (or (= counter number-of-primes) (> n end))
+;             "finished"
+;             (if (prime? n)
+;                 (prime-search-iter (+ n 1) (+ counter 1))
+;                 (prime-search-iter (+ n 1) counter))))
+;     (prime-search-iter start 0))
 
  (search-for-primes 1000000000000 200000000021 3)       ; 1e9 
  (search-for-primes 10000000000000 20000000000061 3)     ; 1e10 
