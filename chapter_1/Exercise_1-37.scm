f4de642b5b8371d213d1109d31f57b8340e0f362
Brandon Ling <brandonvling@gmail.com>
2019-10-09 12:45:45 -0400

    Add solution to exercise 1.37b

--- chapter_1/Exercise_1-37.scm
+++ chapter_1/Exercise_1-37.scm
@@ -10,10 +10,23 @@
                             (- counter 1))))
     (cont-frac-iter 0 k))
 
-(cont-frac (lambda (i) 1.0)
+; (cont-frac (lambda (i) 1.0)
+;            (lambda (i) 1.0)
+;            10) ; returns 0.6179775
+
+; (cont-frac (lambda (i) 1.0)
+;            (lambda (i) 1.0)
+;            20)  ; returns 0.6180339
+
+(define (cont-frac-recursive n d k)
+    (if (= k 1)
+        (/ (n k) (d k))
+        (/ (n k) (+ (/ (n k) (d k)) (cont-frac-recursive n d (- k 1))))))
+
+(cont-frac-recursive (lambda (i) 1.0)
            (lambda (i) 1.0)
            10) ; returns 0.6179775
 
-(cont-frac (lambda (i) 1.0)
+(cont-frac-recursive (lambda (i) 1.0)
            (lambda (i) 1.0)
-           20)
+           20)  ; returns 0.61803398
