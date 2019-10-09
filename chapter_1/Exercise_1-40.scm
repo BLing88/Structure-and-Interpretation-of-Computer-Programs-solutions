af92b61fc8ee993c19d2da6e5088534e3a4ca37c
Brandon Ling <brandonvling@gmail.com>
2019-10-09 14:14:52 -0400

    Add solution to exercise 1.40

---
+++ chapter_1/Exercise_1-40.scm
@@ -0,0 +1,25 @@
+(define (newtons-method g guess)
+    (define (fixed-point f first-guess)
+        (define tolerance 0.00001)
+        (define (close-enough? v1 v2)
+            (< (abs (- v1 v2)) tolerance))
+        (define (try guess)
+            (let ((next (f guess)))
+                (if (close-enough? guess next)
+                    next
+                    (try next))))
+        (try first-guess))
+    (define (newton-transform g)
+        (define (deriv g)
+            (define dx 0.00001)
+            (lambda (x) 
+              (/ (- (g (+ x dx)) (g x))
+              dx)))
+        (lambda (x) 
+          (- x (/ (g x) ((deriv g) x)))))
+    (fixed-point (newton-transform g) guess))
+
+(define (cubic a b c)
+    (lambda (x) (+ (* x x x) (* a x x) (* b x) c)))
+
+(newtons-method (cubic 1 2 3) 1)  ; returns -1.2756822036
