0fb91dc2a48f4a491a1974316929534c4c8d88f4
Brandon Ling <brandonvling@gmail.com>
2019-10-11 19:19:23 -0400

    Add first rectangle representation for 2.3

---
+++ chapter_2/Exercise_2-3.scm
@@ -0,0 +1,102 @@
+(define (make-point x y)
+    (cons x y))
+
+(define (x-point point)
+    (car point))
+
+(define (y-point point)
+    (cdr point))
+
+(define (make-segment start-point end-point)
+    (cons start-point end-point))
+
+(define (start-segment segment)
+    (car segment))
+
+(define (end-segment segment)
+    (cdr segment))
+
+(define (midpoint-segment segment)
+    (define (average x y)
+        (/ (+ x y) 2))
+    (make-point 
+      (average
+        (x-point (start-segment segment)) (x-point (end-segment segment)))
+      (average
+        (y-point (start-segment segment)) (y-point (end-segment segment)))))
+
+(define (print-point p)
+    (newline)
+    (display "(")
+    (display (x-point p))
+    (display ", ")
+    (display (y-point p))
+    (display ")")) 
+
+(define (segment-length segment)
+    (define (dist p1 p2)
+    (define (square x)
+        (* x x))
+    (sqrt 
+    (+ 
+        (square
+        (- (x-point p1) (x-point p2)))
+        (square
+        (- (y-point p1) (y-point p2))))))
+    (dist (x-point segment) (y-point segment)))
+
+;; representation 1: represent rectangles by storing two adjacent sides (represented by segments)
+;; store the shorter side in the car
+;; constructor make-rect and selectors shorter-side, longer-side
+(define (make-rect side-1 side-2)
+    (if (> (segment-length side-1) (segment-length side-2))
+        (cons side-2 side-1)
+        (cons side-1 side-2)))
+
+(define (shorter-side rect)
+    (car rect))
+
+(define (longer-side rect)
+    (cdr rect))
+
+(define (area rect)
+    (* 
+      (segment-length (shorter-side rect)) 
+      (segment-length (longer-side rect))))
+
+(define (perimeter rect)
+    (* 2 
+      (+ 
+        (segment-length (shorter-side rect)) 
+        (segment-length (longer-side rect)))))
+
+(define x (make-rect 
+  (make-segment 
+    (make-point 0 0)
+    (make-point 1 0))
+  (make-segment 
+    (make-point 1 0)
+    (make-point 1 2))))
+
+;; check rotate x by 30 degrees ccw
+(define y (make-rect 
+  (make-segment 
+    (make-point 0 0)
+    (make-point (/ (sqrt 3) 2) (/ 1 2)))
+  (make-segment 
+    (make-point (/ (sqrt 3) 2) (/ 1 2))
+    (make-point (/ (- (sqrt 3) 2) 2) (/ (+ 1 (* 2 (sqrt 3))) 2)))))
+
+(newline)
+(display "perimeter = ")
+(display (perimeter x))
+(newline)
+(display "area = ")
+(display (area x))
+(newline)
+(display "perimeter = ")
+(display (perimeter y))
+(newline)
+(display "area = ")
+(display (area y))
+
