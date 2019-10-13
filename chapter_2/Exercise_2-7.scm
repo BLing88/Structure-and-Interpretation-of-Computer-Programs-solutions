801280c88a79ede4a03f2852ff77cb3788afe9fd
Brandon Ling <brandonvling@gmail.com>
2019-10-13 17:13:07 -0400

    Add solution to exercise 2.7

---
+++ chapter_2/Exercise_2-7.scm
@@ -0,0 +1,12 @@
+(define (make-interval a b)
+    (cons a b))
+
+(define (upper-bound interval)
+    (let ((a (car interval))
+          (b (cdr interval)))
+        (max a b)))
+
+(define (lower-bound interval)
+    (let ((a (car interval))
+          (b (cdr interval)))
+        (min a b)))
