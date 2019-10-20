1a762927185dc016891feeba29332dfd914bf25d
Brandon Ling <brandonvling@gmail.com>
2019-10-20 18:35:31 -0400

    Refactor reverse=iter to not use append

--- chapter_2/Exercise_2-18.scm
+++ chapter_2/Exercise_2-18.scm
@@ -1,14 +1,22 @@
+; (define (reverse x)
+;     (define (reverse-iter x)
+;         (define (append list1 list2)
+;             (if (null? list1)
+;                 list2
+;                 (cons (car list1) (append (cdr list1) list2))))
+;         (if (null? (cdr x))
+;             x
+;             (append (reverse-iter (cdr x)) (list (car x)))))
+;     (reverse-iter x))
+
 (define (reverse x)
-    (define (reverse-iter x)
-        (define (append list1 list2)
-            (if (null? list1)
-                list2
-                (cons (car list1) (append (cdr list1) list2))))
-        (if (null? (cdr x))
-            x
-            (append (reverse-iter (cdr x)) (list (car x)))))
-    (reverse-iter x))
+    (define (reverse-iter x answer)
+        (if (null? x)
+            answer
+            (reverse-iter (cdr x) (cons (car x) answer))))
+    (reverse-iter x '()))
 
 (newline)
-(reverse (list 1 2 3 4))
-(reverse (list "a" "b" "c" "d"))
+(display (reverse (list 1 2 3 4)))
+(newline)
+(display (reverse (list "a" "b" "c" "d")))
