b0e7a99aad6301568fef78980c954af20e53cf4a
Brandon Ling <brandonvling@gmail.com>
2019-10-30 17:41:41 -0400

    Add solution to exercise 2.31

---
+++ chapter_2/Exercise_2-31.scm
@@ -0,0 +1,16 @@
+(define (square x)
+    (* x x))
+
+(define (tree-map f tree)
+    (map (lambda (sub-tree) 
+            (if (pair? sub-tree)
+                (tree-map f sub-tree)
+                (f sub-tree)))
+          tree))
+
+(define (square-tree tree)
+    (tree-map square tree))
+
+(define x (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+(newline)
+(display (square-tree x))
