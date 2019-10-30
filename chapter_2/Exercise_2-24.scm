d6ba19dd3f1ee42f850e0721c065ee4e24396f16
Brandon Ling <brandonvling@gmail.com>
2019-10-30 13:06:04 -0400

    Add solutions to exercises 2.24, 2.25, 2.26

---
+++ chapter_2/Exercise_2-24.scm
@@ -0,0 +1,3 @@
+; (list 1 (list 2 (list 3 4))) returns (1 (2 (3 4)))
+; As a tree it has two main branches, the first is just a leaf (1), the second branches into two
+; subbranches, the first of which is a leaf (2) and the second branches into two leaves (3 4)
