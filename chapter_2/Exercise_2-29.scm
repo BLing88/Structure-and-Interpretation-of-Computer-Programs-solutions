d2fcc4a22ae79f60eda2566dd52b91fe5d74c1ca
Brandon Ling <brandonvling@gmail.com>
2019-10-30 16:58:40 -0400

    Add solution to exercise 2.29c

--- chapter_2/Exercise_2-29.scm
+++ chapter_2/Exercise_2-29.scm
@@ -27,4 +27,22 @@
 (define x (make-mobile (make-branch 2 5) (make-branch 3 11)))
 (define y (make-mobile (make-branch 4 9) (make-branch 7 x)))
 (newline)
-(display (total-weight y))
+; (display (total-weight y))  ;; returns 25
+
+(define (is-balanced? mobile)
+    (define (torque branch)
+        (* (branch-length branch) (total-weight (branch-structure branch))))
+    (define (left-right-balanced? mobile-level)
+        (= (torque (right-branch mobile-level)) (torque (left-branch mobile-level))))
+    (define (is-not-mobile? x)
+        (not (pair? x)))
+    (cond ((is-not-mobile? mobile) true)
+        ((left-right-balanced? mobile)
+            (and (is-balanced? (branch-structure (right-branch mobile))) (is-balanced? (branch-structure (left-branch mobile)))))
+        (else false))
+    )
+
+(define z (make-mobile (make-branch 3 4) (make-branch 2 6)))
+(define w (make-mobile (make-branch 1 y) (make-branch 2 x)))
+(define u (make-mobile (make-branch 2 z) (make-branch 4 5)))
+(display (is-balanced? u))
